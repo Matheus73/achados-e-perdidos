6ba8e0c7b59e3cd7db45b326279584c6344c3410
Matheus <matheuscoding@gmail.com>
2019-12-03 13:02:00 -0300

    Criação da função de busca

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,5 @@ class Idea < ApplicationRecord
     belongs_to :user
     has_many :comments
     mount_uploader :picture, PictureUploader
+    scope :search, ->(query) { where("name like ?", "%#{query}%") }
 end
